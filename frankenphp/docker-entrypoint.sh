cc345117a3ed965cd5a26745a97382d0f1d51682
Yann Bouchereau <yann.bchr@gmail.com>
2024-05-27 17:29:15 +1000

    📦 NEW: uninstall webpack

--- frankenphp/docker-entrypoint.sh
+++ frankenphp/docker-entrypoint.sh
@@ -55,9 +55,6 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 
 	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
 	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
-
-	npm install
-	npm run build
 fi
 
 exec docker-php-entrypoint "$@"
