c86cc7c5f3e7210baf38710625b00c6bb776d836
Yann Bouchereau <yann.bchr@gmail.com>
2024-05-28 16:11:23 +1000

    resintall assetmapper

--- importmap.php
+++ importmap.php
@@ -16,22 +16,4 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
-    'jquery' => [
-        'version' => '3.7.1',
-    ],
-    'diacritics' => [
-        'version' => '1.3.0',
-    ],
-    'fuzzyset' => [
-        'version' => '1.0.7',
-    ],
-    'buffer' => [
-        'version' => '6.0.3',
-    ],
-    'base64-js' => [
-        'version' => '1.5.1',
-    ],
-    'ieee754' => [
-        'version' => '1.2.1',
-    ],
 ];
