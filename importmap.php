80842783c1405390004935e33ae005b69ac8089e
Yann Bouchereau <yann.bchr@gmail.com>
2024-05-28 16:12:17 +1000

    📦 NEW: reinstall assetmapper packages

--- importmap.php
+++ importmap.php
@@ -16,4 +16,22 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
+    'buffer' => [
+        'version' => '6.0.3',
+    ],
+    'base64-js' => [
+        'version' => '1.5.1',
+    ],
+    'ieee754' => [
+        'version' => '1.2.1',
+    ],
+    'diacritics' => [
+        'version' => '1.3.0',
+    ],
+    'fuzzyset' => [
+        'version' => '1.0.7',
+    ],
+    'jquery' => [
+        'version' => '3.7.1',
+    ],
 ];
