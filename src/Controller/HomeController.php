4c570c5b9909108b57840cc026e5bdc07f768b29
Yann Bouchereau <yann.bchr@gmail.com>
2024-06-22 18:32:08 +1000

    📦 NEW: archive

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,39 @@
 
 namespace App\Controller;
 
+use App\Repository\TrackRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class HomeController extends AbstractController
 {
+    private TrackRepository $repository;
+    private SerializerInterface $serializer;
+
+    function __construct(TrackRepository $repository, SerializerInterface $serializer)
+    {
+        $this->repository = $repository;
+        $this->serializer = $serializer;
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/songs/get', name: 'app_home_get_songs')]
+    public function getSongs(): JsonResponse
+    {
+        $tracks = $this->repository->findWithLimit();
+
+        shuffle($tracks);
+
+        $response = $this->serializer->serialize($tracks, 'json');
+
+        return new JsonResponse(base64_encode($response));
     }
 }
