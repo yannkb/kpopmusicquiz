2c70ba961836d1784c9787d50737c75d656364b4
Yann Bouchereau <yann.bchr@gmail.com>
2024-09-19 17:11:08 +1000

    feat: update sql query to get only one song

--- src/Repository/SongRepository.php
+++ src/Repository/SongRepository.php
@@ -17,22 +17,28 @@ class SongRepository extends ServiceEntityRepository
     }
 
     /**
-     * @return Song[] Returns an array of Song objects
+     * @return array Returns a Song object
      */
-    public function findRandom(int $numberOfTracks): array
+    public function findOneRandom(array $excludedSongsIds = []): array
     {
         $conn = $this->getEntityManager()->getConnection();
 
         $sql = '
             SELECT * FROM song s
+            ';
+        if (!empty($excludedSongsIds)) {
+            $sql .= '
+            WHERE id NOT IN (:excludedSongsIds)
+        ';
+        }
+        $sql .= '
             ORDER BY RANDOM()
-            LIMIT :limit OFFSET (SELECT ABS(RANDOM()) % (SELECT COUNT(*) FROM song))
+            LIMIT 1 OFFSET (SELECT ABS(RANDOM()) % (SELECT COUNT(*) FROM song))
             ';
 
-        $resultSet = $conn->executeQuery($sql, ['limit' => $numberOfTracks]);
+        $resultSet = $conn->executeQuery($sql, ['excludedSongsIds' => $excludedSongsIds]);
 
-        // returns an array of arrays (i.e. a raw data set)
-        return $resultSet->fetchAllAssociative();
+        return $resultSet->fetchAssociative();
     }
 
     //    public function findOneBySomeField($value): ?Song
